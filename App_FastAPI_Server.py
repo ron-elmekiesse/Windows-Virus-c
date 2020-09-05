91615254ed785fb59b9f4b55106689ea3804ce38
unknown <ronelmekiesse@gmail.com>
2020-09-05 21:51:18 +0300

    python files

--- App_FastAPI_Server.py
+++ App_FastAPI_Server.py
@@ -199,6 +199,10 @@ async def take_picture(pc_name: str):
 
         await asyncio.sleep(5)  # sleep 5 seconds for other commands to come
 
+
+
+
+
 if __name__ == '__main__':
     # starting the server
     #os.system("uvicorn App_FastAPI_Server:app --reload --port 8000")
